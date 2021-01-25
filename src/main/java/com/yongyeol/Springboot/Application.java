f118e6711daeff4ec9930ba9d2c062b8bd1efaaa
dydfuf <88dydfuf@naver.com>
2021-01-26 02:27:03 +0900

    Initial commit

---
+++ src/main/java/com/yongyeol/Springboot/Application.java
@@ -0,0 +1,11 @@
+package com.yongyeol.Springboot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
