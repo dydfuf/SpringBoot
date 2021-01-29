55b904c0bc7b4210e0b9d223babe5c1132f3cd16
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-01-29 11:19:16 +0900

    Naver Login

--- src/main/java/com/yongyeol/Springboot/web/IndexController.java
+++ src/main/java/com/yongyeol/Springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.yongyeol.Springboot.web;
 
+import com.yongyeol.Springboot.config.auth.LoginUser;
 import com.yongyeol.Springboot.config.auth.dto.SessionUser;
 import com.yongyeol.Springboot.service.posts.PostsService;
 import com.yongyeol.Springboot.web.dto.PostsResponseDto;
@@ -16,13 +17,11 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
