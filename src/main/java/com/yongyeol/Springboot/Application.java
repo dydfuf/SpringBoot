03b030987188358015a311518e96c0494608e565
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-01-30 02:21:45 +0900

    5장

--- src/main/java/com/yongyeol/Springboot/Application.java
+++ src/main/java/com/yongyeol/Springboot/Application.java
@@ -2,9 +2,7 @@ package com.yongyeol.Springboot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
