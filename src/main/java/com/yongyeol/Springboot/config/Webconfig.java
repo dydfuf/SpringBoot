84f275011910178bc64f6a6963b668e7ff76a969
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-02-04 13:42:29 +0900

    CORS 이슈 해결

--- src/main/java/com/yongyeol/Springboot/config/Webconfig.java
+++ src/main/java/com/yongyeol/Springboot/config/Webconfig.java
@@ -4,6 +4,7 @@ import com.yongyeol.Springboot.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +17,10 @@ public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
         argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**");
     }
 }
